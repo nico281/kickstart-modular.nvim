141342a6fd5d2acd82b61d96876e858e9b57aa08
Nicolas <nico281@hotmail.com>
2024-09-11 12:31:01 -0300

    add copilot config

---
+++ lua/custom/plugins/copilot.lua
@@ -0,0 +1,12 @@
+return {
+  'zbirenbaum/copilot.lua',
+  config = function()
+    require('copilot').setup {}
+    vim.keymap.set('i', '<C-l>', function()
+      vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
+    end, { desc = 'Copilot Enter' })
+  end,
+  cmd = 'Copilot',
+  event = 'InsertEnter',
+  lazy = true,
+}
