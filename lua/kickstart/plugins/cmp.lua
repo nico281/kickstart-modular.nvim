cff260e95eca15c6aacc48cd164e3f310dba5b37
Nicolas <nico281@hotmail.com>
2024-11-03 03:31:50 -0300

    add supermaven

--- lua/kickstart/plugins/cmp.lua
+++ lua/kickstart/plugins/cmp.lua
@@ -1,6 +1,7 @@
 return {
   { -- Autocompletion
     'hrsh7th/nvim-cmp',
+    enable = false,
     event = 'InsertEnter',
     dependencies = {
       -- Snippet Engine & its associated nvim-cmp source
