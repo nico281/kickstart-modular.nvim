88d3b31dff4f39ed7cb4fb6c03e7602a4524ad0e
Nicolas <nico281@hotmail.com>
2024-11-07 01:25:51 -0300

    config conform add plugins

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -34,11 +34,12 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
         typescript = { 'prettier' },
+        typescriptreact = { 'prettier' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettier', 'prettierd', stop_after_first = true },
       },
     },
   },
