85ec9fbf2ed899d569b5f7b044728a0890c36c73
Nicolas <nico281@hotmail.com>
2025-02-22 11:29:18 -0300

    config changes.

--- lua/custom/plugins/old-word.lua
+++ lua/custom/plugins/old-word.lua
@@ -4,5 +4,10 @@ return {
   priority = 1000,
   opts = {
     variant = 'olded',
+    highlight_overrides = {
+      Normal = { bg = 'NONE' },
+      NormalNC = { bg = 'NONE' },
+      CursorLine = { bg = '#222128' },
+    },
   },
 }
