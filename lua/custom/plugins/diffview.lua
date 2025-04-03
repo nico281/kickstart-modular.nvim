81234897d773e35ac3ab2b3b3ba426892745d5b1
nico <nico281@hotmail.com>
2025-04-03 00:50:46 -0300

    add keymaps diffview

--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -1,3 +1,9 @@
 return {
   'sindrets/diffview.nvim',
+  keys = {
+    { 'n', '<leader>dv', '<cmd>DiffviewOpen<cr>' },
+    { 'n', '<leader>dvc', '<cmd>DiffviewClose<cr>' },
+    { 'n', '<leader>dvs', '<cmd>DiffviewFileHistory<cr>' },
+    { 'n', '<leader>dvr', '<cmd>DiffviewRefresh<cr>' },
+  },
 }
