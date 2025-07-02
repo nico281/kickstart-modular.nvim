67787b8fc685ec52de67c27f2faa1a7a5ff0bb8d
Nicolas <nico281@hotmail.com>
2025-07-02 01:43:59 -0300

    config colorizer

--- lua/custom/plugins/colorizer.lua
+++ lua/custom/plugins/colorizer.lua
@@ -1,6 +1,21 @@
 return {
   'catgoose/nvim-colorizer.lua',
-  event = 'BufReadPre',
+  event = 'VeryLazy',
   opts = { -- set to setup table
+    lazy_load = true,
   },
+  config = function()
+    require('colorizer').setup {
+      filetypes = { '*' }, -- or limit to specific ones like { "html", "tsx", "svelte", "css" }
+      user_default_options = {
+        RGB = true,
+        RRGGBB = true,
+        names = true,
+        css = true,
+        css_fn = true,
+        mode = 'background', -- or "virtualtext"
+        tailwind = true, -- ✅ enables Tailwind class color support
+      },
+    }
+  end,
 }
