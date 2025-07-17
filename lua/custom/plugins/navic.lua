1a5959738f27155ecc2b2272eb0c236d5e136d55
nico <nico281@hotmail.com>
2025-07-17 13:29:31 -0300

    attach navic to lsp

--- lua/custom/plugins/navic.lua
+++ lua/custom/plugins/navic.lua
@@ -1,4 +1,14 @@
 return {
   'SmiteshP/nvim-navic',
   requires = 'neovim/nvim-lspconfig',
+  lazy = true,
+  event = 'LspAttach',
+  config = function()
+    local function set_highlights()
+      local hl = vim.api.nvim_set_hl
+      hl(0, 'NavicText', { fg = '#ffffff' })
+      hl(0, 'NavicSeparator', { fg = '#888888' })
+    end
+    set_highlights()
+  end,
 }
