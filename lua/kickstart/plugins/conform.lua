6efa0ea3622027f64c7b66f574ab46c5dda95869
nico <nico281@hotmail.com>
2025-07-24 15:46:54 -0300

    aadd ruby lint

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -36,9 +36,9 @@ return {
         typescript = { 'prettier' },
         typescriptreact = { 'prettier' },
         astro = { 'prettier' },
+        ruby = { 'rubyfmt' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
-        --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { 'prettier', 'prettierd', stop_after_first = true },
       },
