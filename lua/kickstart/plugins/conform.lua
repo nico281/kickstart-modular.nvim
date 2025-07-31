a1a785c7fd532a41a08179b97597298d7481617c
nico <nico281@hotmail.com>
2025-07-31 11:47:19 -0300

    fix conform for ruby

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -16,18 +16,30 @@ return {
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
+        local ft = vim.bo[bufnr].filetype
+        local disable_filetypes = { c = true, cpp = true }
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        if disable_filetypes[vim.bo[bufnr].filetype] then
+        if disable_filetypes[ft] then
           return nil
-        else
-          return {
-            timeout_ms = 5000,
-            lsp_format = 'fallback',
-          }
         end
+
+        local conform = require 'conform'
+        local formatters = conform.list_formatters_for_buffer(bufnr)
+        local is_rubocop = false
+
+        for _, formatter in ipairs(formatters) do
+          if formatter.name == 'rubocop' then
+            is_rubocop = true
+            break
+          end
+        end
+
+        return {
+          timeout_ms = is_rubocop and 5000 or 500,
+          lsp_fallback = 'always',
+        }
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
