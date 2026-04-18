0c37b1f160c3d735129224c2c0c654bc35f6156c
Nicolas <nico281@hotmail.com>
2026-04-17 23:59:15 -0300

    tune pretty hover sizing

--- lua/custom/plugins/pretty_hover.lua
+++ lua/custom/plugins/pretty_hover.lua
@@ -1,18 +1,23 @@
+local function hover_opts()
+  return {
+    border = 'rounded',
+    max_width = math.max(80, math.min(110, math.floor(vim.o.columns * 0.55))),
+    max_height = nil,
+    toggle = false,
+    focusable = true,
+    silent = false,
+  }
+end
+
 return {
   'Fildo7525/pretty_hover',
   event = 'LspAttach',
-  opts = {
-    border = 'rounded',
-    max_width = 80,
-    max_height = 20,
-    toggle = true,
-    focusable = true,
-    silent = false,
-  },
+  opts = hover_opts,
   keys = {
     {
       'K',
       function()
+        require('pretty_hover').setup(hover_opts())
         require('pretty_hover').hover()
       end,
       desc = 'LSP: Hover documentation',
